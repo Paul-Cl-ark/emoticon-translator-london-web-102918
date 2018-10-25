82e095c6987426135a380a9f9737e8c72c2b8b5d
Paul Clark <clark.paul123@gmail.com>
2018-10-25 21:22:35 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
-  if
+  tranlator = load_library(file_path)
+  if 
   else puts "Sorry, that emoticon was not found" 
 end
 
